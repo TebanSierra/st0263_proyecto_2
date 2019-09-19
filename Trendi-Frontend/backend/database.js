5c14b7211ec495d0320fa3e203a562ab98ed7c38
Esteban Sierra Munera <esierra@bancolombia.com.co>
2019-09-19 15:13:27 -0500

    Enrutamiento al LB de mongo y persistencia de Mongo

--- Trendi-Frontend/backend/database.js
+++ Trendi-Frontend/backend/database.js
@@ -1,6 +1,6 @@
 const db = require("mongoose");
 
-db.connect("mongodb://mongo:27017/trendi",{useNewUrlParser:true})
+db.connect("mongodb://Mongobalancer-49f3968e4628f967.elb.us-east-1.amazonaws.com:27017/trendi",{useNewUrlParser:true})
     .then(database =>  console.log("db conected"))
     .catch(err =>  console.log(err) );
 
